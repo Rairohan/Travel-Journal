60b72eeb1fb4b5f111844eb762aec31299997a11
rohan <rairohan14303@gmail.com>
2025-12-14 21:39:58 +0545

    final

--- components/App.jsx
+++ components/App.jsx
@@ -1,5 +1,25 @@
-export default function App(){
-    return(
-        <h1>App component</h1>
-    )
+
+import '/src/index.css'
+import Header from '../components/Header'
+import Entry from '../components/Entry'
+import Data from "./Data"
+
+
+export default function App() {
+    const Entrydata = Data.map((entry)=>{
+        return(
+            <Entry
+                key={entry.key}
+                {...entry}
+            />
+        )
+    })
+  return (
+    <>
+      <Header />
+      <main className="container">
+        {Entrydata}
+      </main>
+    </>
+  )
 }
---
+++ components/Data.js
@@ -0,0 +1,53 @@
+import Drjimg from "/public/drj.png"
+export default [{
+    key:1,
+    img:"https://scrimba.com/links/travel-journal-japan-image-url",
+    country:"Japan",
+    location:"https://www.google.com/maps/place/Mount+Fuji",
+    name:"Mount Fuji",
+    date:" 12 Jan, 2023 – 24 Jan, 2023",
+    etext:" Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+                        
+},
+{   
+    key:2,
+    img:"https://scrimba.com/links/travel-journal-australia-image-url",
+    country:"Australia",
+    location:"https://www.google.com/maps?q:Sydney+Opera+House",
+    name:"Sydney Opera House",
+    date:" 27 May, 2023 - 8 Jun, 2023",
+    etext:`The Sydney Opera House is a multi-venue performing arts centre in Sydney.
+              Located on the banks of the Sydney Harbour, 
+              it is often regarded as one of the 20th century's most famous and distinctive buildings.`
+},
+{
+    key:3,
+    img:"https://scrimba.com/links/travel-journal-norway-image-url",
+    country:"Norway",
+    location:"https://maps.app.goo.gl/DF878WVrzyfp7pEA9",
+    name:"Geirangerfjord",
+    date:" 01 Oct, 2024 - 18 Nov, 2024",
+    etext: ` The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway.
+       It is located entirely in the Stranda Municipality.`
+},
+{
+    key:4,
+    img:"/drj.png",
+          country:"India",
+          location:"https://share.google/NBVFAaQkmaNTN7ctl",
+          name:"Batasia Loop",
+          date:" 24 Apr, 2024 - 5 may, 2024",
+          etext:`The Batasia Loop is a spiral railway created to lower the gradient of ascent of the Darjeeling 
+          Himalayan Railway in Darjeeling district of West Bengal, India.`
+},
+{
+    key:5,
+     img:"/pkr.jpg",
+      country:"Nepal",
+      location:"https://maps.app.goo.gl/hPMuYyj4CXVJWTvs5",
+      name:"Phewa Lake",
+      date:" 21 Mar, 2025 - 1 Apr, 2025",
+      etext:` Phewa Lake or Phewa Tal is a freshwater lake in Nepal formerly called Baidam Tal
+       located in the south of the Pokhara Valley that includes Pokhara city and parts of Sarangkot and Kaskikot.`,
+}
+]
